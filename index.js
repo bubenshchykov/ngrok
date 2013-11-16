eaf688b6b62bddb4eddf07cde9f08937b5c47df2
bubenshchykov <abu@e-conomic.com>
2013-11-16 23:51:00 +0200

    full chain test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* exposes localhost to the web via ngrok
 
 usage:
-	require('ngrok').connect(function (url) {
+	var ngrok = require('ngrok');
+	ngrok.connect({port: 80, log: true}, function (url) {
 		console.log('got nkgro url', url);
 	});
+	ngrok.disconnect();
 
 see http://127.0.0.1:4040/http/in to view the webhooks
 see https://ngrok.com/ for details
@@ -12,15 +14,28 @@ see https://ngrok.com/ for details
 var spawn = require('child_process').spawn;
 var path = require('path');
 
+var port = 80;
+var ngrokTunnels = {};
+
 function connect(opts, fn) {
 
+	opts || (opts = {});
+	fn || (fn = function() {});
+
+	if (ngrok) {
+		return;
+	}
+
+	var tunnelUrl;
 	var ngrokBin = getNgrokBin();
-	var ngrok = spawn(ngrokBin, ['-log=stdout', 8080], {cwd: './bin'});
+	var ngrok = spawn(ngrokBin, ['-log=stdout', opts.port || port], {cwd: './bin'});
 
 	ngrok.stdout.on('data', function (data) {
 		var urlMatch = data.toString().match(/Tunnel established at (https..*.ngrok.com)/);
 		if (urlMatch && urlMatch[1]) {
 			tunnelUrl = urlMatch[1];
+			ngrokTunnels[tunnelUrl] = ngrok;
+			log('ngrok: tunnel established at ' + tunnelUrl);
 			return fn(null, tunnelUrl);
 		}
 
@@ -28,14 +43,20 @@ function connect(opts, fn) {
 
 	ngrok.stderr.on('data', function (data) {
 		ngrok.kill();
-		var err = new Error('ngrok: process exited due to error', data.toString().substring(0, 10000));
+		var info = 'ngrok: process exited due to error\n' + data.toString().substring(0, 10000);
+		var err = new Error(info);
+		log(info);
 		return fn(err);
 	});
 
-	ngrok.on('close', function (code) {
-		var err = new Error('ngrok: process exited with a code ' + code);
-		return fn(err);
+	ngrok.on('close', function () {
+		var tunnelInfo = tunnelUrl ? tunnelUrl + ' ' : '';
+		log('ngrok: ' + tunnelInfo + 'disconnected');
 	});
+
+	function log(message) {
+		opts.log && console.log(message);
+	}
 }
 
 function getNgrokBin () {
@@ -47,6 +68,22 @@ function getNgrokBin () {
 	return bins[process.platform] || bins.linux;
 }
 
+function disconnect(tunnelUrl) {
+	if(tunnelUrl) {
+		return killNgrok(tunnelUrl);
+	} else {
+		return Object.keys(ngrokTunnels).forEach(killNgrok);
+	}
+}
+
+function killNgrok(tunnelUrl) {
+	var ngrok = ngrokTunnels[tunnelUrl];
+	ngrok && ngrok.kill();
+	delete ngrokTunnels[tunnelUrl];
+	return;
+}
+
 module.exports = {
-	connect: connect
+	connect: connect,
+	disconnect: disconnect
 };
--- test/ngrok.spec.js
+++ test/ngrok.spec.js
@@ -1,16 +1,86 @@
 var ngrok = require('..');
+var http = require('http');
+var request = require('request');
 
-describe('when connecting the localhost with ngrok', function(){
+var port = 8080;
+var localUrl = 'http://localhost:' + port;
+
+describe('connecting to ngrok', function(){
 	var tunnelUrl;
 
 	before(function (done) {
-		ngrok.connect({}, function(err, url){
+		ngrok.connect({port: port, log: true}, function(err, url){
 			tunnelUrl = url;
 			done();
 		});
-	})
+	});
 
 	it('should return tunnel url', function(){
 		expect(tunnelUrl).not.to.be.empty;
-	})
+	});
+
+	describe('starting simple local server', function() {
+
+		before(function() {
+			http.createServer(function (req, res) {
+				res.writeHead(200);
+				res.end('oki-doki: ' + req.url);
+			}).listen(port);
+		});
+
+		describe('calling local server directly', function() {
+			var respBody;
+
+			before(function(done) {
+				request.get(localUrl + '/local', function (err, resp, body) {
+					respBody = body;
+					done();
+				});
+			});
+
+			it('should return oki-doki', function() {
+				expect(respBody).to.equal('oki-doki: /local');
+			});
+
+			describe('calling local server through ngrok', function() {
+
+				before(function(done) {
+					request.get(tunnelUrl + '/ngrok', function (err, resp, body) {
+						respBody = body;
+						done();
+					});
+				});
+
+				it('should return oki-doki too', function() {
+					expect(respBody).to.equal('oki-doki: /ngrok');
+				});
+
+			});
+
+			describe('disconnecting from ngrok', function () {
+
+				before(function() {
+					ngrok.disconnect();
+				});
+
+				describe('calling local server through discconected ngrok', function() {
+
+					before(function(done) {
+						request.get(tunnelUrl + '/ngrok', function (err, resp, body) {
+							respBody = body;
+							done();
+						});
+					});
+
+					it('should return error message', function() {
+						expect(respBody).to.match(/Tunnel (.)* not found/);
+					});
+
+				});
+
+			});
+
+		});
+
+	});
 });
