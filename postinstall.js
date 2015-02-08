fe4d4492177ed01e9712bf750d00776a0a339769
bubenshchykov <a.bubenshchykov@gmail.com>
2015-02-08 07:46:31 +0400

    minor postinstall cleanup

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,4 @@
-console.error('ngrok - Downloading newest binary...');
+console.error('ngrok - downloading newest binary...');
 
 var os = require('os');
 var https = require('https');
@@ -6,16 +6,17 @@ var fs = require('fs');
 var util = require('util');
 var DecompressZip = require('decompress-zip');
 
+var host = 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?channel=stable&';
 var files = {
-	darwinia32: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=darwin&arch=386&channel=stable',
-	darwinx64: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=darwin&arch=amd64&channel=stable',
-	linuxarm: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=linux&arch=arm&channel=stable',
-	linuxia32: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=linux&arch=386&channel=stable',
-	linuxx64: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=linux&arch=amd64&channel=stable',
-	win32ia32: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=windows&arch=386&channel=stable',
-	win32x64: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=windows&arch=amd64&channel=stable',
-	freebsdia32: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=freebsd&arch=386&channel=stable',
-	freebsdx64: 'https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?os=freebsd&arch=amd64&channel=stable'
+	darwinia32:	host + 'os=darwin&arch=386',
+	darwinx64:	host + 'os=darwin&arch=amd64',
+	linuxarm:	host + 'os=linux&arch=arm',
+	linuxia32:	host + 'os=linux&arch=386',
+	linuxx64:	host + 'os=linux&arch=amd64',
+	win32ia32:	host + 'os=windows&arch=386',
+	win32x64:	host + 'os=windows&arch=amd64',
+	freebsdia32:host + 'os=freebsd&arch=386',
+	freebsdx64:	host + 'os=freebsd&arch=amd64'
 };
 
 var path = __dirname + '/bin/';
@@ -26,32 +27,38 @@ if (!fs.existsSync(path)) {
 var zip = fs.createWriteStream(path + 'ngrok.zip');
 var which = os.platform() + os.arch();
 https.get(files[which], function(response) {
-	response.pipe(zip).on('finish', function() {
-		console.error('ngrok - Zipfile received (' + files[which] + ') ...');
-		unzipFile(path + 'ngrok.zip');
-	});
+	response
+		.pipe(zip)
+		.on('finish', function() {
+			console.log('ngrok - binary downloaded (' + files[which] + ')...');
+			unzipFile(path + 'ngrok.zip');
+		})
+		.on('error', function(e) {
+			console.error('ngrok - error downloading binary', e);
+			process.exit(1);
+		});
 });
 
 function unzipFile(file) {
-	var suffix = os.platform() === 'win32' ? '.exe' : '';
-	var unzipper = new DecompressZip(file);
-	unzipper.extract({ path: path });
-	unzipper.once('error', function(e) {
-		console.error(e);
-		process.exit(1);
-	});
-	unzipper.once('extract', function(log) {
-		if (suffix === '.exe') {
-			fs.writeFileSync(path + 'ngrok.cmd', 'ngrok.exe');
-		}
-		fs.unlinkSync(path + 'ngrok.zip');
-		var target = path + 'ngrok' + suffix;
-		fs.chmodSync(target, 0755);
-		if (fs.existsSync(target) && fs.statSync(target).size > 0) {
-			console.error('ngrok - Binary downloaded.');
-			return;
-		}
-		console.error('ngrok - Binary NOT downloaded.');
-		process.exit(-1);
-	});
+	new DecompressZip(file)
+		.extract({path: path})
+		.once('error', function(e) {
+			console.error('ngrok - error unzipping binary', e);
+			process.exit(1);     
+		})
+		.once('extract', function(log) {
+			var suffix = os.platform() === 'win32' ? '.exe' : '';
+			if (suffix === '.exe') {
+				fs.writeFileSync(path + 'ngrok.cmd', 'ngrok.exe');
+			}
+			fs.unlinkSync(path + 'ngrok.zip');
+			var target = path + 'ngrok' + suffix;
+			fs.chmodSync(target, 0755);
+			if (fs.existsSync(target) && fs.statSync(target).size > 0) {
+				console.log('ngrok - binary unpacked.');
+				return;
+			}
+			console.error('ngrok - error unpacking binary.');
+			process.exit(1);
+		});
 };
