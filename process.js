df8f3ff1ea052b0f17abef39fa1a5852c1cda798
bubenshchykov <a.bubenshchykov@gmail.com>
2018-03-02 20:50:07 +0200

    cleanup

--- process.js
+++ process.js
@@ -10,101 +10,101 @@ const binDir = path.join(__dirname, '/bin');
 let processPromise, activeProcess;
 
 /*
-  ngrok process runs internal ngrok api
-  and should be spawned only ONCE 
-  (respawn allowed if it fails or .kill method called)
+	ngrok process runs internal ngrok api
+	and should be spawned only ONCE 
+	(respawn allowed if it fails or .kill method called)
 */
 
-async function startProcessSafe(opts) {
-  if (processPromise) return processPromise; 
-  try {
-    processPromise = startProcess(opts);
-    return await processPromise;
-  }
-  catch(ex) {
-    processPromise = null;
-    throw ex;
-  }
+async function startProcessOnce(opts) {
+	if (processPromise) return processPromise; 
+	try {
+		processPromise = startProcess(opts);
+		return await processPromise;
+	}
+	catch(ex) {
+		processPromise = null;
+		throw ex;
+	}
 }
 
 async function startProcess (opts) {
-  const start = ['start', '--none', '--log=stdout']
-  if (opts.region) start.push('--region=' + opts.region);
-  if (opts.configPath) start.push('--config=' + opts.configPath);
+	const start = ['start', '--none', '--log=stdout']
+	if (opts.region) start.push('--region=' + opts.region);
+	if (opts.configPath) start.push('--config=' + opts.configPath);
 
-  const ngrok = spawn(bin, start, {cwd: binDir})
-  
-  let resolve, reject;
-  const apiUrl = new Promise((res, rej) => {   
-    resolve = res;
-    reject = rej;
-  });
+	const ngrok = spawn(bin, start, {cwd: binDir})
+	
+	let resolve, reject;
+	const apiUrl = new Promise((res, rej) => {   
+		resolve = res;
+		reject = rej;
+	});
 
-  ngrok.stdout.on('data', data => {
-    const msg = data.toString();
-    const addr = msg.match(ready)
-    if (addr) {
-      resolve(`http://${addr[1]}`);
-    } else if (msg.match(inUse)) {
-      reject(new Error(msg.substring(0, 10000)));
-    }
-  });  
+	ngrok.stdout.on('data', data => {
+		const msg = data.toString();
+		const addr = msg.match(ready)
+		if (addr) {
+			resolve(`http://${addr[1]}`);
+		} else if (msg.match(inUse)) {
+			reject(new Error(msg.substring(0, 10000)));
+		}
+	});  
 
-  ngrok.stderr.on('data', data => {
-    const msg = data.toString().substring(0, 10000);
-    reject(new Error(msg));
-  })
+	ngrok.stderr.on('data', data => {
+		const msg = data.toString().substring(0, 10000);
+		reject(new Error(msg));
+	})
 
-  process.on('exit', async () => await killProcess());
+	process.on('exit', async () => await killProcess());
 
-  try {
-    const url = await apiUrl;
-    activeProcess = ngrok;
-    return url;      
-  }
-  catch(ex) {
-    ngrok.kill();
-    throw ex;
-  }
-  finally {
-    ngrok.stdout.removeAllListeners('data');
-    ngrok.stderr.removeAllListeners('data');
-  }
+	try {
+		const url = await apiUrl;
+		activeProcess = ngrok;
+		return url;      
+	}
+	catch(ex) {
+		ngrok.kill();
+		throw ex;
+	}
+	finally {
+		ngrok.stdout.removeAllListeners('data');
+		ngrok.stderr.removeAllListeners('data');
+	}
 }
 
 function killProcess ()  {
-  if (!activeProcess) return;
-  return new Promise(resolve => {
-    activeProcess.on('exit', () => {
-      processPromise = null;
-      activeProcess = null;
-      resolve();
-    });
-    activeProcess.kill();
-  });
+	if (!activeProcess) return;
+	return new Promise(resolve => {
+		activeProcess.on('exit', () => {
+			processPromise = null;
+			activeProcess = null;
+			resolve();
+		});
+		activeProcess.kill();
+	});
 }
 
 async function setAuthtoken (token, configPath) {
-  const authtoken = ['authtoken', token]
-  if (configPath) authtoken.push('--config=' + configPath);
+	const authtoken = ['authtoken', token]
+	if (configPath) authtoken.push('--config=' + configPath);
 
-  const ngrok = spawn(bin, authtoken, {cwd: binDir});
+	const ngrok = spawn(bin, authtoken, {cwd: binDir});
 
-  const killed = new Promise((resolve, reject) => {
-    ngrok.stdout.once('data', () => resolve());
-    ngrok.stderr.once('data', () => reject(new Error('cant set authtoken')));
-  });
+	const killed = new Promise((resolve, reject) => {
+		ngrok.stdout.once('data', () => resolve());
+		ngrok.stderr.once('data', () => reject(new Error('cant set authtoken')));
+	});
 
-  try {
-    return await killed;
-  }
-  finally {
-    ngrok.kill();
-  }
+	try {
+		return await killed;
+	}
+	finally {
+		ngrok.kill();
+	}
 }
 
 module.exports = {
-  startProcess: startProcessSafe,
-  killProcess,
-  setAuthtoken
+	startProcess: startProcessOnce,
+	killProcess,
+	setAuthtoken
 };
