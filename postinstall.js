190dfae0e8b75bc65eccac14fd1e1b73bdab7be7
Mohamed Feddad <mrf345@gmail.com>
2020-04-26 07:19:28 +0300

    Fix executable path in windows 10 bash. (#173)

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,25 @@
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const pkgName = require('./package.json').name;
+
 require('./download')(err => {
+  // Fix default executable path on Windows Git Bash
+  if (!err && process.env.MSYSTEM && os.release().includes('10')) {
+    const exeFile = path.resolve(process.env.APPDATA, path.join('npm', pkgName));
+
+    if (fs.existsSync(exeFile)) {
+      const parsedContent = fs
+        .readFileSync(exeFile)
+        .toString()
+        .replace(
+          `"$basedir/node_modules/${pkgName}/bin/${pkgName}"`,
+          `"winpty" "$basedir/node_modules/${pkgName}/bin/${pkgName}.exe"`
+        );
+
+      fs.writeFileSync(exeFile, parsedContent);
+    }
+  }
+
   process.exit(err ? 1 : 0);
 });
