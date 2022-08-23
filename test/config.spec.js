af3304487c060a236efc0e8b206d65b2d384ee4e
Phil Nash <philnash@gmail.com>
2022-08-23 22:33:58 +1000

    Adds test for config upgrade function

--- test/config.spec.js
+++ test/config.spec.js
@@ -1,8 +1,11 @@
 const { expect } = require("chai");
+const { parse } = require("yaml");
 const ngrok = require("..");
 
 const { join } = require("path");
+const { readFileSync, writeFileSync, unlinkSync } = require("fs");
 const configPath = join("test", "fixtures", "old.yml");
+const backedUpConfigPath = join("test", "fixtures", "old.yml.v1.bak");
 const port = 8080;
 
 describe("a config file without a version", () => {
@@ -10,7 +13,7 @@ describe("a config file without a version", () => {
     await ngrok.kill();
   });
 
-  it("throws an error", async () => {
+  it("throws an error when trying to connect", async () => {
     let error;
     try {
       await ngrok.connect({
@@ -22,5 +25,28 @@ describe("a config file without a version", () => {
     }
     expect(error).not.to.be.undefined;
     expect(error).to.match(/Error reading configuration file/);
+  });
+
+  describe("with the upgradeConfig function", () => {
+    let configContents;
+    beforeEach(() => {
+      configContents = readFileSync(configPath, "utf-8");
+    });
+    afterEach(() => {
+      writeFileSync(configPath, configContents, "utf-8");
+      try {
+        unlinkSync(backedUpConfigPath);
+      } catch (e) {}
+    });
+
+    it("can be upgraded", async () => {
+      const oldConfig = parse(readFileSync(configPath, "utf-8"));
+      expect(oldConfig.version).to.be.undefined;
+      expect(oldConfig.region).to.be.undefined;
+      await ngrok.upgradeConfig({ configPath });
+      const config = parse(readFileSync(configPath, "utf-8"));
+      expect(config.version).to.eq("2");
+      expect(config.region).to.eq("us");
+    });
   });
 });
